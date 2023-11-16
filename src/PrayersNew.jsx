409ee7c44abaaf5157fa97dbb67d2be12e43ea2d
vanwilson <144718641+vanwilson@users.noreply.github.com>
2023-11-16 16:01:28 -0600

    added form from bootstrap to add new prayer

--- src/PrayersNew.jsx
+++ src/PrayersNew.jsx
@@ -1,16 +1,31 @@
 export function PrayersNew() {
   return (
     <div id="prayers-new">
-      <h1>Prayer Request</h1>
-      <form>
-        <div>
-          Name: <input type="text" />
+      <div className="row g-3 align-items-center">
+        <div className="col-auto">
+          <label htmlFor="inputPassword6" className="col-form-label">
+            Title
+          </label>
         </div>
-        <div>
-          Prayer: <input type="text" />
+        <div className="col-auto">
+          <input type="password" id="inputPassword6" className="form-control" aria-describedby="passwordHelpInline" />
         </div>
-        <div></div>
-      </form>
+        <div className="col-auto">
+          <span id="passwordHelpInline" className="form-text">
+            Your prayer within 6 words.
+          </span>
+        </div>
+      </div>
+      <div className="row g-3 align-items-center">
+        <div className="col-auto">
+          <label htmlFor="inputPassword6" className="col-form-label">
+            Body
+          </label>
+        </div>
+        <div className="col-auto">
+          <input type="password" id="inputPassword6" className="form-control" aria-describedby="passwordHelpInline" />
+        </div>
+      </div>
     </div>
   );
 }
